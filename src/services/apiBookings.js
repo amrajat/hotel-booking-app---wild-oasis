941447fed0d98bfb49592e9092b703d515848742
Rajat Panwar <ablognet@gmail.com>
2023-11-03 08:48:58 +0530

    feat. pagination with prefetch

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,3 +1,4 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
@@ -5,23 +6,33 @@ export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
-  //filter
+
+  // FILTER
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
-  //sort
+
+  // SORT
   if (sortBy)
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
-  if (error) {
-    console.error(error);
-    throw new Error("Booking not found");
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
   }
 
-  return data;
+  const { data, error, count } = await query;
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return { data, count };
 }
 
 export async function getBooking(id) {
